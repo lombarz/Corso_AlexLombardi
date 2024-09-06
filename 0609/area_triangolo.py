4f8b59f9764e270e2bf57fcf84eafdd38493f933
lombarz <137717516+lombarz@users.noreply.github.com>
2024-09-06 13:04:05 +0200

    Add files via upload

--- 0609/area_triangolo.py
+++ 0609/area_triangolo.py
@@ -1,38 +1,34 @@
-def area_trinagolo(b,a):
-    area=b*a/2
-    return area
-def area_rettangolo(b,a):
-    area=b*a
-    return area
-def area_quadrato(l):
-    area=l**2
-    return area
-def controllo(stop):
+def area_trinagolo():
+    altezza=int(input("valore altezza: "))
+    base=int(input("valore altezza: "))
+    area=base*altezza/2
+    print("l'area vale", area)
+def area_rettangolo():
+    altezza=int(input("valore altezza: "))
+    base=int(input("valore altezza: "))
+    area=base*altezza
+    print("l'area vale", area)
+def area_quadrato():
+    lato=int(input("valore lato: "))
+    area=lato**2
+    print("l'area vale", area)
+def controllo():
    chc2=input("Vuoi fare un'altra operazione? s n")
    if chc2=='n':
-      stop='True'
-   return stop
+    return 'True'
+   else:
+    return 'False'
    
 
 stop='False'
 while stop=='False':
  chc=int(input("Di che oggetto vuoi calcolare l'area? 1=triangolo, 2=rettangolo, 3=quadrato"))
  if chc==1:
-   altezza=int(input("valore altezza: "))
-   base=int(input("valore altezza: "))
-   output= area_trinagolo(base,altezza)
-   print("L'area è ", output)
-   controllo(stop)
+   area_trinagolo()
  elif chc==2:
-   altezza=int(input("valore altezza: "))
-   base=int(input("valore altezza: "))
-   output= area_rettangolo(base,altezza)
-   print("L'area è ", output)
-   controllo(stop)
+   area_rettangolo()
  elif chc==3:
-   lato=int(input("valore lato: "))
-   output= area_quadrato(lato)
-   print("L'area è ", output)
-   controllo(stop)
+   area_quadrato()
  else:
-    print("Valore errate, ripetere selezione")
+    print("Valore errato")
+ stop=controllo()
