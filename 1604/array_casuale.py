5599414db519d75946157b3f1c015470aee759af
lombarz <137717516+lombarz@users.noreply.github.com>
2024-09-16 15:11:49 +0200

    Update array_casuale.py

--- 1604/array_casuale.py
+++ 1604/array_casuale.py
@@ -5,7 +5,7 @@ casuali=np.random.randint(10,50,(20,))
 print(casuali)
 x_cas=casuali[:10]
 print(x_cas)
-lastv_cas=casuali[15:20]
+lastv_cas=casuali[-5:]
 print(lastv_cas)
 middle_cas=casuali[5:15]
 print(middle_cas)
